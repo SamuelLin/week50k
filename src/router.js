a65327a8762b6743a8af8f114e57018aab981788
Samuel <samuelhsnu@gmail.com>
2019-01-28 21:37:41 +0800

    vue router

--- src/router.js
+++ src/router.js
@@ -34,6 +34,10 @@ const router = new Router({
         isPublic: false
       },
       component: () => import('./views/Leaderboard.vue')
+    },
+    {
+      path: '/*',
+      redirect: '/'
     }
   ]
 })
